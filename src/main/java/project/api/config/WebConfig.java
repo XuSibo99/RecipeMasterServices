60f6367703d3da6dd7e3d4dc4b454457851be5cb
Sibo <markxusibo@gmail.com>
2025-04-02 22:54:43 +0800

    chore(cors): implement dynamic CORS configuration based on environment

--- src/main/java/project/api/config/WebConfig.java
+++ src/main/java/project/api/config/WebConfig.java
@@ -1,16 +1,22 @@
 package project.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${CORS_FRONTEND_URL}")
+    private String frontEndUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/graphql")
+                .allowedOrigins(frontEndUrl)
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
